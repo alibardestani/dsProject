9a19eae3f61f3701bac0b659e85366a974d4cde6
aliwdxa <alibardestani201796@gmail.com>
2023-07-05 09:59:09 +0330

    add history file foe frequency and a little TUI

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,75 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main extends Trie {
     public static void main(String[] args) {
         List<String> words = Read.readWordsFromFile("D:\\learn\\Shiraz\\Ds\\DsProject\\src\\words.txt");
         List<String> wordsReverse = Read.readReverceWordFromFile("D:\\learn\\Shiraz\\Ds\\DsProject\\src\\words.txt");
+        ArrayList<String> History = new ArrayList<>();
         Trie trie = new Trie();
         Trie trieReverse = new Trie();
+
+        Scanner input = new Scanner(System.in);
 
         for (String word : words) {
             trie.insert(word);
         }
-        for (String wordd : wordsReverse){
-            trieReverse.insert(wordd);
+        for (String word : wordsReverse){
+            trieReverse.insert(word);
+        }
+
+        boolean flag = false;
+        System.out.println("Please Enter String");
+        String[] str = input.nextLine().split(" ");
+        while (!flag){
+            for(int i = 0; i<str.length; i++){
+                for(String word : str){
+                    System.out.print(word+" ");
+                }
+                System.out.println("\n===> "+str[i]+"\n1-AutoComplete\n2-Suggestions");
+                int choice = input.nextInt();
+                switch (choice) {
+                    case 1 -> {
+                        for (int it = 0; it < trie.autocomplete(str[i]).size(); it++) {
+                            System.out.println(it + "\t" + trie.autocomplete(str[i]).get(it));
+                        }
+                        int SeChoice = input.nextInt();
+                        str[i] = trie.autocomplete(str[i]).get(SeChoice);
+                        System.out.println();
+                        History.add(str[i]);
+                    }
+                    case 2 -> {
+                        System.out.println(21);
+                        ArrayList<ArrayList<Object>> suggestions = ConnectTwoArray(trie.Suggestions(str[i]), trieReverse.Suggestions(String.valueOf(new StringBuilder(str[i]).reverse())));
+
+                        // Print the suggestions with their corresponding indices
+                        for (int index = 0; index < suggestions.size(); index++) {
+                            ArrayList<Object> suggestion = suggestions.get(index);
+                            System.out.println(index + "\t" + suggestion.get(0) + "\t" + suggestion.get(1));
+                        }
+
+                        // Get user's choice
+                        int userChoice = input.nextInt();
+
+                        // Check if the user's choice is within the valid range
+                        if (userChoice >= 0 && userChoice < suggestions.size()) {
+                            String chosenString = (String) suggestions.get(userChoice).get(0);
+                            System.out.println("User chose: " + chosenString);
+                            str[i] = chosenString;
+                            History.add(chosenString);
+                        } else {
+                            System.out.println("Invalid choice.");
+                        }
+                    }
+                    default -> flag = true;
+                }
+            }
+            flag = true;
+        }
+        for(String word : str){
+            System.out.print(word+" ");
         }
 //        System.out.println("Trie contains 'apple': " + trie.search("tes"));
 //        System.out.println(trie.autocomplete("ti"));
@@ -21,6 +77,7 @@ public class Main extends Trie {
         //for(Object word : ConnectTwoArray(trie.Suggestions("ple"),trieReverse.Suggestions(String.valueOf(new StringBuilder("ple").reverse())))){
             //System.out.println(word);
         //}
+        Write.Write(History,"history.txt");
 
 
     }
@@ -29,15 +86,15 @@ public class Main extends Trie {
         ArrayList<ArrayList<Object>> combined = new ArrayList<>(pre);
         combined.addAll(suf);
 
-        for(Object i : pre){
-            System.out.println(i);
-        }
+//        for(Object i : pre){
+//            System.out.println(i);
+//        }
         reverseStrings(suf);
-        System.out.println("*");
-        for (Object y : suf){
-            System.out.println(y);
-        }
-        System.out.println("******\t\t******");
+//        System.out.println("*");
+//        for (Object y : suf){
+//            System.out.println(y);
+//        }
+//        System.out.println("******\t\t******");
         // Sort the combined ArrayList based on the Integer values in ascending order
         combined.sort(Comparator.comparingInt(row -> (int) row.get(1)));
 
